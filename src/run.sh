e17e2e9bce648834ee840f286b72a8df0ee0f399
liranr23 <liranr23@gmail.com>
2017-06-04 09:48:06 -0700

    changed run.sh

--- src/run.sh
+++ src/run.sh
@@ -3,11 +3,11 @@
 #Copying config to the usr share folder,
 sudo cp ./movies.conf /usr/share/nginx/movies.conf
 
-sudo nginx -c movies.conf &>/dev/null
+sudo nginx -c movies.conf 2> /dev/null
 
 pid=$( pgrep -x "fcgi" )
 
-if [ $pid != "" ] ;then
+if [ "$pid" != "" ] ;then
     kill -KILL "$pid"
 fi
 spawn-fcgi -p 8000 fcgi
