752225df1863b183660172e0ca3e56cdce59e819
Wo1v3r <johnleon24@gmail.com>
2017-06-06 17:08:41 +0300

    Removed unecessary files, added machine destination to /distLocal to run.sh

--- src/compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ *.cpp -std=c++11 -lfcgi -lfcgi++ -lsqlite3 -o fcgi 
--- src/run.sh
+++ src/run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+#Setting root of server to dist
+dist=$(pwd)
+dist="${dist%/*}"/distLocal
+sed -i "/<root>/c\root $dist ;" movies.conf
+
 #Copying config to the usr share folder,
 sudo cp ./movies.conf /usr/share/nginx/movies.conf
 
 sudo nginx -c movies.conf 2> /dev/null
 
-pid=$( pgrep -x "fcgi" )
+pid=$( pgrep "fcgi" )
 
 if [ "$pid" != "" ] ;then
-    kill -KILL "$pid"
+    sudo kill -KILL "$pid"
 fi
-spawn-fcgi -p 8000 fcgi
+sudo spawn-fcgi -p 8000 fcgi
 
